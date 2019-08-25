6b8f328f0d16011d5b2854c0023c521db57f2e01
J.M. de la Rosa Trevin <delarosatrevin@gmail.com>
2019-08-26 01:13:49 +0200

    Added protocol to correct motion of Tilt-Series

--- motioncorr/protocols/__init__.py
+++ motioncorr/protocols/__init__.py
@@ -24,4 +24,11 @@
 # *
 # **************************************************************************
 
-from protocol_motioncorr import ProtMotionCorr
+from .protocol_motioncorr import ProtMotionCorr
+
+try:
+    from .protocol_ts_motioncor import ProtTsMotionCorr
+except ImportError:
+    raise ImportError(
+        'To use a Tomography protocol scipion-em-tomo plugin is required.'
+        ' See https://github.com/scipion-em/scipion-em-tomo for further details')
