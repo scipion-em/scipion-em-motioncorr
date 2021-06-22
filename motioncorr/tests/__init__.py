b87decd82ecd8309713e59bd06d1860d684bf9f5
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2021-06-22 12:20:24 +0200

    * Creating a tests to TestMotioncor2AlignMovies * Checking if tomo plugin is installed

--- motioncorr/tests/__init__.py
+++ motioncorr/tests/__init__.py
@@ -1,2 +1,8 @@
 
 from .test_protocols_motioncor2 import TestMotioncor2AlignMovies
+
+try:
+    from .test_protocols_tomo import TestMotioncor2TiltSeriesAlignMovies
+except Exception as e:
+    if not "'tomo'" in str(e):
+        raise e
